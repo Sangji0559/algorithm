9b82209853e60c5af88c72a2be69d462df04f9cf
sangji0559 <gkgktkdwl@gmail.com>
2023-11-27 12:31:23 +0900

    change parameter in volume calculate

--- al_project.py
+++ al_project.py
@@ -3,18 +3,19 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('input.csv')
-dft=df.T
+dft = df.T
 
 def calculate_volume(matrix):
     # 행렬의 부피 계산
     det = np.linalg.det(matrix.T @ matrix)
-    volume = np.sqrt(abs(det))
+    volume =np.sqrt(abs(det))
     return volume
 
 def find_max_volume(data):
     max_volume = 0
-    for _ in range(100):
-        sample = data.sample(frac=0.3)
+    for _ in range(2):
+        sample = data.sample(frac=0.5, replace=False)
+        print(sample)
         volume = calculate_volume(sample)
         if(max_volume<volume):
             max_volume = volume
@@ -25,6 +26,5 @@ start_time = time.time()
 max_volume = find_max_volume(dft)
 end_time = time.time()
 
-print("단위 벡터 결과:")
-print(max_volume)
+print("결과: ", max_volume)
 print("소요 시간:", end_time - start_time, "초")
