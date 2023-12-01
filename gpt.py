b6dccdfb7a6a090fa3f619330e08def6670e1dff
sangji0559 <gkgktkdwl@gmail.com>
2023-12-01 10:34:50 +0900

    dd

--- gpt.py
+++ gpt.py
@@ -1,60 +1,85 @@
-import pandas as pd
-import numpy as np
-import random
-import time
+import heapq
 
-def calculate_volume(matrix):
-    # 행렬의 부피 계산
-    det = np.linalg.det(matrix.T @ matrix)
-    volume = np.sqrt(abs(det))
-    return volume
+coordinates = [(0,3),(7,5),(6,0),(4,3),(1,0),(5,3),(2,2),(4,1)]
 
-m = 500  # 원하는 샘플 개수
+def prim(points):
+    n = len(points)
+    visited = [False] * n
+    heap = [(0, 0)]  # (weight, vertex)
+    mst = []
 
-# '0' 열을 기준으로 샘플링
-sampled_data = input_data['0'].sample(m, replace=False).values
+    while heap:
+        weight, u = heapq.heappop(heap)
+
+        if not visited[u]:
+            visited[u] = True
+            for v in range(n):
+                if not visited[v]:
+                    x1, y1 = points[u]
+                    x2, y2 = points[v]
+                    distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2)
+                    heapq.heappush(heap, (distance, v))
+                    mst.append((u, v, distance))
+
+    return mst
+
+def mst_to_graph(mst_edges):
+    graph = {}
+    for edge in mst_edges:
+        u, v, weight = edge
+        if u not in graph:
+            graph[u] = []
+        if v not in graph:
+            graph[v] = []
+        graph[u].append((v, weight))
+        graph[v].append((u, weight))
+    return graph
+
+def tsp_from_mst(mst_edges, start):
+    mst_graph = mst_to_graph(mst_edges)
+
+    # Eulerian circuit 찾기
+    euler_circuit = []
+
+    def dfs(node):
+        nonlocal euler_circuit
+        for neighbor, _ in mst_graph[node]:
+            if (node, neighbor) not in euler_circuit and (neighbor, node) not in euler_circuit:
+                euler_circuit.append((node, neighbor))
+                dfs(neighbor)
+
+    dfs(start)
+
+    # 중복되는 도시 제거하고 경로 구성
+    tsp_path = [euler_circuit[0][0]]
+    for _, v in euler_circuit:
+        tsp_path.append(v)
+
+    unique_tsp_path = list(dict.fromkeys(tsp_path))
+
+    return unique_tsp_path
 
 
-def find_max_volume(input_file, m, num_samples):
-    # CSV 파일에서 데이터 로드
-    data = pd.read_csv(input_file)
-    
-    start_time = time.time()
-    
-    max_volume = 0
-    max_index = None
-    
-    for _ in range(num_samples):
-        print("h")
-        # 무작위로 샘플된 인덱스 (전체 집합 크기보다 작거나 같도록 설정)
-        sample_indices = random.sample(range(data.shape[0]), min(m, data.shape[0]))
-        
-        # 행렬 A 구성
-        A = data.iloc[sample_indices, 1:].values
-        
-        # 볼륨 계산
-        volume = calculate_volume(A)
-        
-        # 최대 볼륨 갱신
-        if volume > max_volume:
-            max_volume = volume
-            max_index = sample_indices
-    
-    end_time = time.time()
-    
-    # 결과 출력
-    print(f"최대 볼륨: {max_volume}")
-    print(f"해당 벡터의 인덱스: {max_index}")
-    print(f"실행 시간: {end_time - start_time} 초")
 
-# 입력 파일 경로
-input_file_path = 'input.csv'
+prim_mst = prim(coordinates)
 
-# 추출할 벡터의 개수 (예: 50개)
-m = 10
-
-# 샘플링 횟수 (조절 가능)
-num_samples = 1000
-
-# 함수 호출
-find_max_volume(input_file_path, m, num_samples)
+tsp_path_prim = tsp_from_mst(prim_mst, 0)
+tsp_prim = []
+for i in tsp_path_prim:
+    if i == 0:
+        tsp_prim.append('A')
+    if i == 1:
+        tsp_prim.append('B')
+    if i == 2:
+        tsp_prim.append('C')
+    if i == 3:
+        tsp_prim.append('D')
+    if i == 4:
+        tsp_prim.append('E')
+    if i == 5:
+        tsp_prim.append('F')
+    if i == 6:
+        tsp_prim.append('G')
+    if i == 7:
+        tsp_prim.append('H')
+print("Prim TSP 경로:", tsp_prim)
