186c74c74eef005d1c373d1b8a86cb0823876473
sangji0559 <gkgktkdwl@gmail.com>
2023-12-04 15:51:45 +0900

    algorithm project

--- gpt.py
+++ gpt.py
@@ -1,34 +1,28 @@
+import time
 import pandas as pd
 import numpy as np
 
-def calcVolume(mat):
-    return np.sqrt(np.abs(np.linalg.det(np.dot(mat, mat.T))))
+df = pd.read_csv('input.csv') # 20*10000
+dft = df.T # 10000*20
 
-def maximize_volume(matrix, k):
-    U, S, Vt = np.linalg.svd(matrix, full_matrices=False)
-
-    # 대각 행렬 직접 생성
-    diag_S = np.diag(S[:k])
-
-    # make approx matrix
-    approx_matrix = np.dot(U[:, :k], np.dot(diag_S, Vt[:k, :]))
-
-    volume = calcVolume(approx_matrix)
-
+def calculate_volume(matrix):
+    # 행렬의 부피 계산
+    det = np.linalg.det(matrix @ matrix.T)
+    volume = np.sqrt(abs(det))
     return volume
 
-# CSV 파일에서 데이터 읽기
-inp = pd.read_csv('input.csv', header=None)
+def sample_matrix(): # 20*20 행렬 만들기
+    sampled_col = list(np.argmax(np.abs(df.values), axis=1))
+    
+    sampled_column = df.columns[sampled_col]
+    sampled_data = df[sampled_column]
+    
+    return sampled_data, sampled_col
 
-matrix = inp.values  # DataFrame을 NumPy 배열로 변환
 
-best_k = 0
-max_volume = 0
-
-num_columns_to_use = 300  # 사용할 열 수 조정
-current_volume = maximize_volume(matrix[:, :num_columns_to_use], 20)
-
-if current_volume > max_volume:
-    max_volume = current_volume
-
-print("최대 부피:", max_volume)
+start_time = time.time()
+data, index = sample_matrix()
+print("sample_matrix 결과 : ", calculate_volume(data))
+print("matrix index : ", list(index))
+end_time = time.time()
+print("소요 시간:", end_time - start_time, "초")
--- ngpt.py
+++ ngpt.py
@@ -1,34 +1,32 @@
-import pandas as pd
 import time
+import pandas as pd
 import numpy as np
-import random
 
+df = pd.read_csv('input.csv')  # 20*10000
+dft = df.T  # 10000*20
 
-def calcVolume(mat):
-    return np.sqrt(np.abs(np.linalg.det(mat @ mat.T)))
-
-def maximize_volume(matrix, k):
-    U, S, Vt = np.linalg.svd(matrix, full_matrices=False)
-
-    # make approx matrix
-    approx_matrix = np.dot(U[:, :k], np.dot(np.diag(S[:k]), Vt[:k, :]))
-
-    volume = calcVolume(approx_matrix)
-
+def calculate_volume(matrix):
+    # 행렬의 부피 계산
+    det = np.linalg.det(matrix.T @ matrix)
+    volume = np.sqrt(abs(det))
     return volume
 
-inp = pd.read_csv('input.csv')
-matrix = inp.values
+def sample_matrix_custom():
+    # 예제: 각 열의 합이 큰 순서대로 20개의 행 선택
+    col_sums = df.abs().sum(axis=0)
+    sampled_cols = col_sums.nlargest(1000).index
+    sampled_data = df[sampled_cols]
+    print(sampled_data)
+    sampled_col = list(np.argmax(np.abs(sampled_data.values), axis=1))
+    sampled_column = df.columns[sampled_col]
+    sampled_data = df[sampled_column]
+    print(sampled_data)
+    
+    return sampled_data, sampled_col
 
 
-max_volume = 0
 start_time = time.time()
-current_volume = maximize_volume(matrix[:, :300], 20)
-
-if current_volume > max_volume:
-    max_volume = current_volume
-
+print("sample_matrix_custom 결과: ", calculate_volume(sample_matrix_custom()))
 end_time = time.time()
 
-print(max_volume)
-print(end_time-start_time)
+print("소요 시간:", end_time - start_time, "초")
